b0822c821a8f49a063026ff2da66ff3f03d24edf
Apollo Lo <apollol@bu.edu>
2021-04-02 23:00:52 -0400

    two new testing cases

--- test_cases.py
+++ test_cases.py
@@ -15,11 +15,28 @@ test_list = {
         },
          "starttls": True,
     },
-    "attack1": {
+    #trying to spoof dkim
+    #add a comma right after selector, and check if dkim can still query the correct address
+    "attack1": { 
         "ehlo": b'EHLO pujanpaudel.net\r\n',
         "mailfrom": b'mail from: <test@pujanpaudel.net>\r\n',
         "rcptto": b'rcpt to: <'+receiver_email.encode() +b'>\r\n',
-        "dkim" : {"d":b"pujanpaudel.net", "s":b"postal-J2v2Mw", "sign_header": b"From: <admin@legitimate.com>"},
+        "dkim" : {"d":b"pujanpaudel.net", "s":b"postal-J2v2Mw,", "sign_header": b"From: <admin@legitimate.com>"},
+         "data": {
+            "from_header": b"From: FAKE NAME <fake@fake.com>\r\n",
+            "to_header": b"To: <victim@victim.com>\r\n",
+            "subject_header": b"Subject: New Subjects\r\n",
+            "body": b"This is a test message.\r\n",
+        },
+         "starttls": True
+    },
+    #trying to spoof demarc
+    #add nextline in d tag to see if dkim and dmarc can query the right domain
+    "attack2": { 
+        "ehlo": b'EHLO pujanpaudel.net\r\n',
+        "mailfrom": b'mail from: <test@pujanpaudel.net>\r\n',
+        "rcptto": b'rcpt to: <'+receiver_email.encode() +b'>\r\n',
+        "dkim" : {"d":chr(10).encode() + b"pujanpaudel.net", "s":b"postal-J2v2Mw", "sign_header": b"From: <admin@legitimate.com>"},
          "data": {
             "from_header": b"From: FAKE NAME <fake@fake.com>\r\n",
             "to_header": b"To: <victim@victim.com>\r\n",
